d957d1374f171153ac58c712a52e963eb44598bd
MosaabGaber <mosaabahmedgaber@gmail.com>
2026-04-16 01:15:04 +0200

    Update banner and navbar

--- src/components/CountdownBanner.tsx
+++ src/components/CountdownBanner.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 
-const TARGET_DATE = new Date('2026-04-15T23:59:59+02:00').getTime();
+const TARGET_DATE = new Date('2026-04-17T23:59:59+02:00').getTime();
 
 function getTimeLeft() {
   const now = Date.now();
