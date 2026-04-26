69a741d6c9b6f7e50893d7b049685615c47dc2ab
MosaabGaber <mosaabahmedgaber@gmail.com>
2026-04-26 14:31:19 +0300

    fix: add eventID to Purchase pixel event on thank-you page

--- src/components/CountdownBanner.tsx
+++ src/components/CountdownBanner.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 
-const TARGET_DATE = new Date('2026-04-25T23:59:59+02:00').getTime();
+const TARGET_DATE = new Date('2026-04-27T23:59:59+02:00').getTime();
 
 function getTimeLeft() {
   const now = Date.now();
